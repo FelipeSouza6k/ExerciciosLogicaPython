bcaa8a63444b3d539b82495393dd0de3cde2fd33
Felipe Souza <125583116+FelipeSouza6k@users.noreply.github.com>
2025-07-12 10:12:01 -0300

    att

---
+++ Faccat/Antecessor.py
@@ -0,0 +1,10 @@
+print("DIGITE UM NÚMERO PARA DESCOBRIR SEU ANTECESSOR")
+while  True:
+    entrada = input("Digite o valor: ")
+    try:
+        numero = int(entrada)
+        break
+    except ValueError:
+        print("Entrada Inválida, digite novamente")
+print( f"Antecessor de {numero} é : {numero - 1}")
+
---
+++ Faccat/Retangulo.py
@@ -0,0 +1,20 @@
+print("Calcular área de um retángulo")
+
+while True:
+    entradaAltura = input("Digite a altura: ")
+    try:
+        altura = int(entradaAltura)
+        break
+    except ValueError:
+        print ("Valor da da altura inválida")
+        
+while True:
+    entradaBase = input("Digite a base")
+    try:
+        base = int(entradaBase)
+        break
+    except ValueError:
+        print("Valor da base inválida")
+         
+print(f"Área do retangulo é: {base * altura}")
+

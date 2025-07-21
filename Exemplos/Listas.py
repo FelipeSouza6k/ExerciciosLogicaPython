c074a445e3dfc4b3536d3acc4d20bd57e0ddc19c
Felipe Souza <125583116+FelipeSouza6k@users.noreply.github.com>
2025-07-21 13:08:33 -0300

    att

---
+++ Exemplos/Listas.py
@@ -0,0 +1,14 @@
+nomes = []
+
+print("Bem-vindo ao adicionador de nomes!")
+
+while True:
+    nomeNovo = input("Digite um nome para adicionar à lista (ou 'sair' para encerrar): ")
+    if nomeNovo.lower() == 'sair':
+        nomes.sort()#Faz a lista ficar em ordem alfabética
+        print("Encerrando programa. Nomes adicionados em ordem alfabética:", nomes)
+        break
+    else:
+        nomes.append(nomeNovo)
+        nomes.sort()
+        print(f"'{nomeNovo}' adicionado. Lista atual: {nomes}")        
